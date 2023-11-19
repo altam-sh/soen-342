da2c1d5a12bff530f641a2c574957ac2a8a1b61a
Shamma Markis <95432232+shamma13@users.noreply.github.com>
2023-11-19 16:02:59 -0500

    Add files via upload

---
+++ iteration1/code/Read.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Read extends Registry{
+	
+	ArrayList<Read_item> Read;
+	
+	public Read() {
+		new ArrayList<Read_item>();
+	}
+	
+	public void CreateRead_item(Sensor sensor, Temperature temp) {
+		Read.add(new Read_item(sensor, temp));
+	}
+	
+	public ArrayList<Read_item> getRead() {
+		return Read;
+	}
+	
+
+}
---
+++ iteration1/code/Read_item.java
@@ -0,0 +1,27 @@
+
+public class Read_item {
+	private Sensor sensor;
+	private Temperature temp;
+	
+	public Read_item(Sensor sensor, Temperature temp) {
+		this.sensor = sensor;
+		this.temp = temp;
+	}
+
+	public Sensor getS() {
+		return sensor;
+	}
+
+	public void setS(Sensor sensor) {
+		this.sensor = sensor;
+	}
+
+	public Temperature getT() {
+		return temp;
+	}
+
+	public void setT(Temperature temp) {
+		this.temp = temp;
+	}
+
+}
