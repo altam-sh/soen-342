bab6f6c13aef4e758195bd29bd27ca447185a2da
mzmddi <110358757+mzmddi@users.noreply.github.com>
2023-11-18 20:45:19 -0500

    created Map.java and wrote the implementation inside

---
+++ iteration1/code/Map.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Map extends Registry{
+    ArrayList<Map_item> map;
+
+    Map(){
+        this.map = new ArrayList<Map_item>;
+        //add default maps in the registry since registry can only see both sensor and map
+    }
+    public void CreateMap_item(Sensor sensor, Location location) {
+
+        Map.add(new Map_item(sensor, location));
+    }
+    public ArrayList<Map_item> getMap(){
+        return Map;
+    }
+}
